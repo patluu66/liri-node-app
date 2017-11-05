edb3a7ad0b9a9ea948e1f023789abdccebb2e858
Patrick Luu <patluu66@gmail.com>
2017-11-05 00:24:50 -0700

    add files

---
+++ keys.js
@@ -0,0 +1,33 @@
+// console.log('this is loaded');
+
+// var twitterKeys = {
+//   consumer_key: 'F7CW1UpnYde6YWMjvtrXxHwlv',
+//   consumer_secret: 'pENB1Ezpr1tixiI230p4pseTz1H8HCcRZTgnclMm5LGHlwsOa9',
+//   access_token_key: '49535583-uOasGmrMr5Wn19FC0LXTztS1ymYUODwRZjq0ZSswO',
+//   access_token_secret: '8byY6zICD1CRf7g1pQbxyW8AD1tEqViZkjxmVwlp1sslw',
+// }
+
+// module.exports = twitterKeys;
+
+
+
+
+
+// console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: 'F7CW1UpnYde6YWMjvtrXxHwlv',
+    consumer_secret: 'pENB1Ezpr1tixiI230p4pseTz1H8HCcRZTgnclMm5LGHlwsOa9',
+    access_token_key: '49535583-uOasGmrMr5Wn19FC0LXTztS1ymYUODwRZjq0ZSswO',
+    access_token_secret: '8byY6zICD1CRf7g1pQbxyW8AD1tEqViZkjxmVwlp1sslw'
+}
+
+module.exports = twitterKeys;
+
+
+
+
+  //   consumer_key: 'F7CW1UpnYde6YWMjvtrXxHwlv',
+  //   consumer_secret: 'pENB1Ezpr1tixiI230p4pseTz1H8HCcRZTgnclMm5LGHlwsOa9',
+  //   access_token_key: '49535583-uOasGmrMr5Wn19FC0LXTztS1ymYUODwRZjq0ZSswO',
+  //   access_token_secret: '8byY6zICD1CRf7g1pQbxyW8AD1tEqViZkjxmVwlp1sslw'
